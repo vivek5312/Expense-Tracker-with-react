5884218556f0472d3d01f4211643d5a880d11606
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-07-06 23:58:28 +0530

    add arrow function

---
+++ src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+
+import "./Expense.css";
+
+function ExpenseItem(props) {
+  return (
+    
+    <div className="Expense-item">
+      <ExpenseDate date={props.date}/>
+      <ExpenseDetails amount={props.amount} location={props.location} title={props.title}/>
+    </div>
+
+  );
+}
+export default ExpenseItem;
