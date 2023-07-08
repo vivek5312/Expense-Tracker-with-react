e16a55e39502271642d86de0b6f80d172772780f
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-07-08 10:38:47 +0530

    Add change Expense button

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,22 +1,32 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
 import "./Expense.css";
 
-const ExpenseItem=(props) =>{
-const ClickHandler=()=>{
-  console.log("cliked")
-}
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+  console.log("expense item is excuted");
+  const ClickHandler = () => {
+    setTitle("Updated");
+    console.log(title);
+  };
+
+  const ChangeExpense = () => {
+    setAmount(100);
+  };
   return (
-    
     <div className="Expense-item">
-      <ExpenseDate date={props.date}/>
-      <ExpenseDetails amount={props.amount} location={props.location} title={props.title}/>
-      <button className="change-Title" onClick={ClickHandler}>Change Title</button>
-      <button className="delbtn" >Delete Expense</button>
-
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails amount={amount} location={props.location} title={title} />
+      <button className="change-Title" onClick={ClickHandler}>
+        Change Title
+      </button>
+      <button className="delbtn" onClick={ChangeExpense}>
+        Change-Expense
+      </button>
     </div>
-
   );
-}
+};
 export default ExpenseItem;
