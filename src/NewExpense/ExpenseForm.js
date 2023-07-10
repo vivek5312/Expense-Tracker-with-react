3b414ce221604986761e2b6058b3395114f0e0f8
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-07-10 22:11:00 +0530

    Adding toggle to button

--- src/NewExpense/ExpenseForm.js
+++ src/NewExpense/ExpenseForm.js
@@ -1,74 +1,73 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm=(props)=>{
-   const[entertitle,setEnteredTitle]=useState(' ');
-    const[enterAmount,setEneteredAmount]=useState(' ');
-    const [enterDate,setEnteredDate]=useState(' ');
+const ExpenseForm = (props) => {
+  const [entertitle, setEnteredTitle] = useState("");
+  const [enterAmount, setEneteredAmount] = useState("");
+  const [enterDate, setEnteredDate] = useState("");
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
- 
-/* const [userInput,setUserInput]=useState({
-    entertitle:'',
-    enterAmount:'',
-    enterDate:'',
-}); */
-    const TitleHandler=(event)=>{
-        setEnteredTitle(event.target.value)
-      /* setUserInput({
-        ...userInput,
-        entertitle: event.target.value
-      }) */
-    /*   setUserInput((prevState)=>{
-        return{...prevState,entertitle:event.target.value}
-      }) */
-    }
-    const AmountHandler=(event)=>{
-        setEneteredAmount(event.target.value);
-       /*  etUserInput({
-            ...userInput,
-            enterAmount: event.target.value
-          })s */
-    }
-   const DateHandler=(event)=>{
+  const TitleHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const AmountHandler = (event) => {
+    setEneteredAmount(event.target.value);
+  };
+
+  const DateHandler = (event) => {
     setEnteredDate(event.target.value);
-   /*  setUserInput({
-        ...userInput,
-        enterDate: event.target.value
-      }) */
-   }
-   const SubmitHandler=(event)=>{
+  };
+
+  const SubmitHandler = (event) => {
     event.preventDefault();
-    const ExpenseData={
-        title:entertitle,
-        amount:enterAmount,
-        date:new Date(enterDate),
-    }
-    props.onSaveExpenseData(ExpenseData)
-   setEneteredAmount(' ');
-   setEnteredDate(' ');
-   setEnteredTitle(' ');
-   
-   }
+    const ExpenseData = {
+      title: entertitle,
+      amount: enterAmount,
+      date: new Date(enterDate),
+    };
+    props.onSaveExpenseData(ExpenseData);
+    setEneteredAmount("");
+    setEnteredDate("");
+    setEnteredTitle("");
+  };
+
+  const toggleFormVisibility = () => {
+    setIsFormVisible((prevIsFormVisible) => !prevIsFormVisible);
+  };
+
+  if (!isFormVisible) {
     return (
-        <form onSubmit={SubmitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                <label>Title</label>
-                <input type="text"value={entertitle} onChange={TitleHandler}></input>
-                </div>
-                <div className="new-expense__control">
-                <label>Amount</label>
-                <input type="number" value={enterAmount} onChange={AmountHandler}></input>
-                </div>
-                <div className="new-expense__control">
-                <label>Date</label>
-                <input type="date" value={enterDate} onChange={DateHandler}></input>
-                </div>
-            </div>
-           <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
-           </div>
-        </form>
-    )
-}
+      <button type="button" onClick={toggleFormVisibility}>
+        Add Expense
+      </button>
+    );
+  }
+
+  return (
+    <form onSubmit={SubmitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" value={entertitle} onChange={TitleHandler}></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" value={enterAmount} onChange={AmountHandler}></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" value={enterDate} onChange={DateHandler}></input>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="button" onClick={toggleFormVisibility}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button> 
+      </div>
+    </form>
+  );
+};
+
 export default ExpenseForm;
