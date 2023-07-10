3274874995c50c2acc441ef7d643ca8c9d59d903
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-07-10 21:46:16 +0530

    Add rendering method on filter

--- src/App.js
+++ src/App.js
@@ -1,106 +1,4 @@
-/* import React,  { useState } from "react";
-import ExpenseItem from "./Components/Expenses/ExpenseItem";
-import NewExpenses from "./NewExpense/NewExpenses";
-import ExpensesFilter from "./Components/Expenses/ExpensesFilter"; 
 
-
-
-
-   const Dummyexpense = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount:2000,
-      date: new Date(2023, 7, 6),
-      location: "Kadodara",
-    },
-    {
-      id: "e2",
-      title:"T.V",
-      amount:10000,
-      date: new Date(2023, 7, 6),
-      location: "Gangadhara",
-    },
-    {
-      id: "e3",
-      title: "Mobile",
-      amount:15000,
-      date: new Date(2023, 7, 7),
-      location: "Surat",
-    },
-    {
-      id: "e4",
-      title: "Watch",
-      amount:1500,
-      date: new Date(2023, 7, 8),
-      location: "Sachin",
-    },
-    {
-      id: "e5",
-      title: "Bike",
-      amount:1500,
-      date: new Date(2023, 7, 8),
-      location:"Kadodra",
-    }
-  ];
- 
-  function App(props) {
-    const [Expense,setExpense]=useState(Dummyexpense);
-    const AddExpenseHandler = (expenseData) => {
-      setExpense((prevExpenses) => {
-        return [...prevExpenses, expenseData];
-      });
-    };
-    const[FilteredYear,setFilter]=useState('2023');
-
-    const handleFilterChange=(selectedYear)=>{
-      setFilter(selectedYear);
-    }; 
-
-    const filteredExpense=props.item.filter(expense=>{
-      return expense.date.getfullyear().toString()===FilteredYear
-    })
-
-/*     
-  const expenseItem=[];
-  for(let i=0;i<Expense.length;i++){
-    expenseItem.push(
-
-      
-    <ExpenseItem
-     key={Expense[i].id} 
-     title={Expense[i].title} 
-     amount={Expense[i].amount} 
-     date={Expense[i].date}/*  location={Expense[i].location} */
-
- 
- 
-
- 
-
-/*   return (
-    <div className="App">
-    
-     <NewExpenses onAddExpense={AddExpenseHandler}></NewExpenses>
-     <ExpensesFilter value={FilteredYear} onChangeFilter={handleFilterChange} />
-
-     {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          location={expense.location}
-        />
-      ))}
-  
-    
-     
-    </div>
-  );
-}
-
-export default App; */
 import React, { useState } from "react";
 import ExpenseItem from "./Components/Expenses/ExpenseItem";
 import NewExpenses from "./NewExpense/NewExpenses";
@@ -118,28 +16,28 @@ const Dummyexpense = [
     id: "e2",
     title: "T.V",
     amount: 10000,
-    date: new Date(2023, 7, 6),
+    date: new Date(2022, 7, 6),
     location: "Gangadhara",
   },
   {
     id: "e3",
     title: "Mobile",
     amount: 15000,
-    date: new Date(2023, 7, 7),
+    date: new Date(2020, 7, 7),
     location: "Surat",
   },
   {
     id: "e4",
     title: "Watch",
     amount: 1500,
-    date: new Date(2023, 7, 8),
+    date: new Date(2019, 7, 8),
     location: "Sachin",
   },
   {
     id: "e5",
     title: "Bike",
     amount: 1500,
-    date: new Date(2023, 7, 8),
+    date: new Date(2022, 7, 8),
     location: "Kadodra",
   },
 ];
@@ -161,23 +59,31 @@ function App(props) {
   const filteredExpenses = expenses.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+let ExpenseContent= <p>No expense data.</p>;
+if(filteredExpenses.length>0){
+ ExpenseContent= filteredExpenses.map((expense) => (
+    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+      location={expense.location}
+    />
+  ))
+}
+
 
   return (
     <div className="App">
-      <NewExpenses onAddExpense={addExpenseHandler} />
-      <ExpensesFilter selected={filteredYear} onChangeFilter={handleFilterChange} />
-
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          location={expense.location}
-        />
-      ))}
-    </div>
-  );
-}
-
+    <NewExpenses onAddExpense={addExpenseHandler} />
+    <ExpensesFilter selected={filteredYear} onChangeFilter={handleFilterChange} />
+    {ExpenseContent}
+      
+  </div>
+)
+  }
 export default App;
+
+
+
+
